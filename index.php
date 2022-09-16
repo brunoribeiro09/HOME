5e70958c77ac1ee154fc698e4be4fb99c36119fe
brunoribeiro09 <brunoribeiropereiradasilva@gmail.com>
2022-09-16 18:17:31 -0300

    sistema de login ok

--- index.php
+++ index.php
@@ -1,46 +1,31 @@
 <?php 
-include_once('config.php');
+include('config.php');
 
-if(isset($_POST['nome']) and isset($_POST['senha'])){
-    if (strlen($_POST['nome']) == 0){
-        echo "Preencha o seu nome de Usuario";
-    } else if (strlen($_POST['senha']) == 0){
-        echo "Preencha a sua senha";
-    } else {
+if(isset($_POST['nome']) and isset($_POST['senha'])) {
 
-        $nome = $conexao->real_escape_string($_POST['nome']);
-        $senha = $conexao->real_escape_string($_POST['senha']);
+    $nome = $conexao->real_escape_string($_POST['nome']);
+    $senha = $conexao->real_escape_string($_POST['senha']);
 
-        $sql_code = "SELECT * FROM usuarios WHERE nome = '$nome' and senha = '$senha'";
-        $sql_query = $conexao->query($sql_code) or die("Falha na execução do código SQL : " . $conexao->error);
+    $sql_code = "SELECT * FROM usuarios WHERE nome = '$nome' and senha = '$senha'";
+    $sql_query = $conexao->query($sql_code) or die ("Falha na conexão!");
 
-        $quantidade = $sql_query->num_rows;
+    $quantidade = $sql_query->num_rows;
 
-        if ($quantidade == 1) {
+    if($quantidade == 1) {
 
-            $usuario = $sql_query->fetch_assoc();
+        $usuario = $sql_query->fetch_assoc();
 
-            if(!isset($_SESSION)){
-                session_start();
-            }
-
-            $_SESSION['user'] = $_SESSION ['id'];
-            $_SESSION['nome'] = $_SESSION ['nome'];
-
-            header("Location: painel.php");
-
-        } else {
-            echo "Falha ao logar ! E-mail ou senha incorretos";
+        if(!isset($_SESSION)){
+            session_start();
         }
+
+        $_SESSION['user'] = $_SESSION['id'];
+        $_SESSION['nome'] = $_SESSION['nome'];
+
+        header("Location: https://www.youtube.com/watch?v=Cb0iyx_MQPM&ab_channel=R19Clips");
     }
-
-
-
 }
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -48,17 +33,66 @@ if(isset($_POST['nome']) and isset($_POST['senha'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN</title>
+    <title>Arcade</title>
+    <link rel="stylesheet" href="estilos/estilodapagina.css">
+    <link rel="stylesheet" href="estilos/arealogin.css">
 </head>
 <body>
-    <form action="index.php" method="post">
+    <header>
 
-    Nome :
-    <input type="text" name="nome" id=""> 
-    Senha :
-    <input type="password" name="senha" id="">
+    </header>
+    <main>
+        <section class="borda">  <!--Primeira Borda Externa-->
+            <div class="barra2"></div> <!--Segunda Barra Preta-->
+            <div class="barra1"></div> <!--Primeira Barra Preta-->
+            
 
-    <input type="submit" value="ENTRAR">
-    </form>    
+            <section class="arcade">
+
+                <div class="bordaarcade">
+                    <div class="bordaarcade2"></div><!--Barrinha Preta - Esquerda -->
+
+                    <div class="bordainterna">
+                        <p>SEJA BEM-VINDO !</p> 
+                    </div>
+                    
+                    <div class="bordaarcade1"> </div><!--Barrinha Preta - Direita -->  
+                </div>
+
+                <section class="partesuperior">
+                    <div class="supleft"></div>
+                    <div class="supright"></div>
+                    <div class="supcenter"></div>
+                </section>
+            </section>
+
+            <section class="partesuperior"> <!--PARTE DO DISPLAY - CIMA E EM BAIXO-->
+
+                <div class="left"></div>
+                <div class="center"></div>
+                <div class="right"></div>
+                <div class="bordadisplay">
+                    
+                <section class="area-login">
+                    <div class="login">
+                        <form method="POST">
+                            <img src="Mangekyou_Sharingan_Kakashi.svg.png" alt="">
+                            <input type="text" name="nome" placeholder="nome de usuario" autofocus required>
+                            <input type="password" name="senha" placeholder="insira a sua senha" required minlength="8">
+                        <input type="submit" value="entrar">
+                        </form>
+                        <p>Ainda não tem uma conta ? <a href="formulario.php" target="_blank">CRIAR CONTA</a></p>
+                    </div>
+                </section>
+                <picture>
+            
+                    
+                </picture>
+
+                </div>
+
+            </section>
+        </section>
+    </main>
 </body>
 </html>
